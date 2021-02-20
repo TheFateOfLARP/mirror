533d55496246557ff8be78e30d5ccd178c0cadcc
pmalaya <polina.malaya@inplat.ru>
2021-02-20 15:32:56 +0300

    pictures

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,52 +1,79 @@
 let ws;
 
 function connect() {
-    let HOST = location.origin.replace(/^http/, 'ws');
-    ws = new WebSocket(HOST);
-    ws.onclose = () => {
-        setTimeout(() => {
-            console.log('PANIC WebSocket died! Reconecting...');
-            connect()
-        }, 3000);
-    }
+  let HOST = location.origin.replace(/^http/, "ws");
+  ws = new WebSocket(HOST);
+  ws.onclose = () => {
+    setTimeout(() => {
+      console.log("PANIC WebSocket died! Reconecting...");
+      connect();
+    }, 3000);
+  };
 }
 
 connect();
 
-sendMsg = (item) => {
+sendMsg = (item, type) => {
+  if (type === "img") {
+    sendFile();
+  } else {
     const text = item.parentElement.children[0].innerHTML.trim();
-    prepMsg(text);
-}
+    console.log(item.parentElement.innerHTML);
+    prepMsg(text, type);
+  }
+  // console.log({item, type})
+};
 
-wsSend = (text) => {
-    ws.send(JSON.stringify(text));
-}
+wsSend = (text, type) => {
+    ws.send(JSON.stringify(text), type);
+  
+};
 
 sendCustomMsg = (item) => {
-    const text = item.parentElement.children[0].value.trim();
-    words = text.split(' ').filter((n) => n != '');
-    i = 0;
-    words.forEach(w => {
-        if (!(i%20) && i != 0) {
-            words.splice(i, 0, '{NEWLINE}');
-        }
-        i++
-    });
-    
-    prepMsg(words.join(' '));
-}
+  const text = item.parentElement.children[0].value.trim();
+  words = text.split(" ").filter((n) => n != "");
+  i = 0;
+  words.forEach((w) => {
+    if (!(i % 20) && i != 0) {
+      words.splice(i, 0, "{NEWLINE}");
+    }
+    i++;
+  });
 
-prepMsg = (text) => {
+  prepMsg(words.join(" "));
+};
+
+prepMsg = (text, type) => {
     const array = text.split(/{NEWLINE}/gi);
     i = 0;
-    wsSend(array[i]);
+    wsSend(array[i], type);
     next = () => {
-        if( ++i < array.length) {
-            setTimeout(() => {
-                wsSend(array[i]);
-                next();
-            }, array[i-1].length*100 + 3000);
-        }
-    }
+      if (++i < array.length) {
+        setTimeout(() => {
+          wsSend(array[i]);
+          next();
+        }, array[i - 1].length * 100 + 3000);
+      }
+    };
     next();
-}
+  
+};
+function sendFile() {
+    var file = document.getElementById("filename").files[0];
+
+    var reader = new FileReader();
+
+    var rawData = new ArrayBuffer();
+    reader.onload = function (e) {
+      rawData = e.target.result;
+      console.log(rawData);
+
+      ws.send(rawData);
+
+      // alert("the File has been transferred.");
+    };
+
+    reader.readAsArrayBuffer(file);
+  }
+
+
--- assets/js/client.js
+++ assets/js/client.js
@@ -1,33 +1,75 @@
 function connect() {
-    let HOST = location.origin.replace(/^http/, 'ws');
-    let ws = new WebSocket(HOST);
-    ws.onclose = () => {
-        setTimeout(() => {
-            console.log('PANIC WebSocket died! Reconecting...');
-            connect()
-        }, 3000);
+  let HOST = location.origin.replace(/^http/, "ws");
+  let ws = new WebSocket(HOST);
+  ws.onclose = () => {
+    setTimeout(() => {
+      console.log("PANIC WebSocket died! Reconecting...");
+      connect();
+    }, 3000);
+  };
+
+  ws.onmessage = (msg) => {
+    const type = typeof msg.data;
+    if (type === "object") {
+      // если приходит картинка
+      var image = new Image();
+      image.src = URL.createObjectURL(msg.data);
+      $("#image").attr("src", image.src).animate_Img();
+    } else {
+      // если приходит текст
+      payload = JSON.parse(msg.data);
+      $("h3").text(payload);
+
+      $("#fittext").animate_Text();
     }
 
-    ws.onmessage = (msg) => {
-        payload = JSON.parse(msg.data)
-        $("h3").text( payload );
-        
-        $('#fittext').animate_Text();
-    };
+    // document.body.appendChild(image);
+
+    // image.src = message.data;
+    // var urlObject = URL.createObjectURL(message.data);
+    // image.src = urlObject;
+    // console.log(msg)
+    // console.log(type)
+
+    // payload = JSON.parse(msg.data)
+    // $("h3").text( payload );
+
+    // $('#fittext').animate_Text();
+  };
 }
 
-$(document).ready(function(){
-    connect();
-    $.fn.animate_Text = function() {
-        var string = this.text();
-        return this.each(function(){
-            var $this = $(this);
-            $this.html(string.replace(/./g, '<span class="new">$&</span>'));
-            $this.find('span.new').each(function(i, el){
-                setTimeout(function(){ $(el).addClass('div_opacity'); }, 100 * i);
-            });
-        });
-    };
+$(document).ready(function () {
+  connect();
+  $.fn.animate_Text = function () {
+    var string = this.text();
+    return this.each(function () {
+      var $this = $(this);
+      $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+      $this.find("span.new").each(function (i, el) {
+        setTimeout(function () {
+          $(el).addClass("div_opacity");
+        }, 100 * i);
+      });
+    });
+  };
 
-    $('#fittext').animate_Text();
+  $("#fittext").animate_Text();
+
+  $.fn.animate_Img = function () {
+    $( "#image" ).animate({
+      width: "60%",
+      opacity: 0.8,
+    }, 4000, function() {
+      $( "#image" ).animate({
+        opacity: 0,
+      }, 9000, function() {
+        $( "#image" ).animate({
+          
+          width: "0%",
+        })
+
+      });
+    });
+    
+  };
 });
